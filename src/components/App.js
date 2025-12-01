21ebc4c6091b8d649530c740c6edcca76f5e6add
vedantkherade <vedantkherade76@gmail.com>
2025-12-01 23:14:43 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
-
 import React from "react";
+import { useState } from "react";
 import './../styles/App.css';
+import TodoList from "./TodoList";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, title: "Learn React", completed: false },
+    { id: 2, title: "Build a React app", completed: false },
+    { id: 3, title: "Deploy the React app", completed: false },
+  ]);
+
+  const handleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, completed: true } : todo
+      )
+    );
+  };
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+      <TodoList todos={todos} handleComplete={handleComplete}/>
     </div>
   )
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function TodoList({ todos, handleComplete }) {
+  return (
+    <div>
+      <h2>Child Component</h2>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.title}{" "}
+            {!todo.completed && (
+              <button onClick={() => handleComplete(todo.id)}>
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
